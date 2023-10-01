33b04045f84f2c45b433cc088df905ea8ac71a25
Mateo Lievano Tejedor <128878423+Boxxingboll@users.noreply.github.com>
2023-10-01 13:48:56 -0500

    Arreglos generales, en la parte inicial.

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/* Video */
+const video = document.getElementById("video-bg");
+
+function moveVideo(event) {
+  const xOffset = -event.clientX / 0;
+  const yOffset = -event.clientY / 0;
+  video.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
+}
+
+document.addEventListener("mousemove", moveVideo);
+
+/* Navegación */
+
+function redirectToCloudArchitectPage() {
+  window.location.href = "http://localhost/roads/CloudEngineer.html";
+}
+
+function redirectToCloudEngineerPage() {
+  window.location.href = "http://localhost/roads/CloudEngineer.html";
+}
+
+function redirectToCloudAnalystPage() {
+  window.location.href = "http://localhost/roadMap/roads/CloudAnalyst.html";
+}
+function redirectToCloudAdministratorPage() {
+  window.location.href = "http://localhost/roadMap/roads/CloudAdministrator.html";
+}
+
+function redirectToCloudDeveloperPage() {
+  window.location.href = "http://localhost/roadMap/roads/CloudDeveloper.html";
+}
